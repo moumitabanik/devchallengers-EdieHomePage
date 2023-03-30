d4167004436fa1d0095d08ab12582f5fecde646a
Moumita <53989363+moumitabanik@users.noreply.github.com>
2023-03-30 15:54:18 +0530

    Add files via upload
    Final commit

---
+++ app/main.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector(".hamburger");
+const involve = document.querySelector("#dropdown_involve");
+const dropdown = document.querySelector("#dropdown");
+const content = document.querySelector(".dropdown_content");
+
+menuBtn.addEventListener('click',()=>{
+    var openmenu= document.querySelector(".dropdown_involve");
+    if(openmenu){
+        involve.classList.remove("dropdown_involve");
+        dropdown.classList.remove("dropdown");
+        content.style.display = "none";
+    }
+    else{
+        involve.classList.add("dropdown_involve");
+        dropdown.classList.add("dropdown");
+        content.style.display = "flex";
+    }
+});
+
+
